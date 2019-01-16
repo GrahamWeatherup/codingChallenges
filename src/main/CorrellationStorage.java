4e3996d075a6f0ce3d2a81c83ba4f6eed8e43ba1
graham.weatherup <graham.weatherup@cgi.com>
2019-01-16 17:31:55 +0000

    Fixed Jamie's correllation problem

--- src/main/CorrellationStorage.java
+++ src/main/CorrellationStorage.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class CorrellationStorage {
 
 
-
+    private double[][] correllations;
+    private HashMap<Integer, Integer> idMap;
+    private Click[] clicks;
 
     public static void main(String[] args){
 
@@ -13,17 +15,56 @@ public class CorrellationStorage {
     }
 
     void run(){
-        Click[] clicks = getTestData(1000,5);
+        clicks = getTestData(1000,5);
 
-        HashMap<MyKey,Double> correllations = new HashMap<MyKey,Double>();
+        correllations = calculateCorrelations(clicks);
 
-        for (int i=0; i<clicks.length;i++){
-            for (int j=i+1; j<clicks.length;j++){
-                correllations.put(new MyKey(i,j),correllation(clicks[i].wave,clicks[j].wave));
-            }
+        double correllation_3_5 = getCorrellationByIndex(3, 5);
+        double correllation_5_3 = getCorrellationByIndex(5, 3);
+
+        if (correllation_3_5==correllation_5_3){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
         }
 
+        double correllationById_5_6 = getCorrellationByIds(1005, 1006);
+        double correllationById_6_5 = getCorrellationByIds(1006, 1005);
 
+        if (correllationById_5_6==correllationById_6_5){
+            System.out.println("PASS by ID");
+        }else {
+            System.out.println("FAIL by ID");
+        }
+    }
+
+    private double getCorrellationByIndex(int i, int j) {
+        if (j>i){
+            return correllations[j][i];
+        }else {
+            return correllations[i][j];
+        }
+    }
+
+    private double getCorrellationByIds(int id1, int id2){
+        int indexa = idMap.get(id1);
+        int indexb = idMap.get(id2);
+        return getCorrellationByIndex(indexa,indexb);
+    }
+
+
+    private double[][] calculateCorrelations(Click[] clicks) {
+        correllations = new double[clicks.length][];
+        idMap=new HashMap<>();
+        for (int i=0; i<clicks.length;i++){
+            idMap.put(clicks[i].id,i);
+            correllations[i] = new double[i+1]; // remove the '+1' here and the '=' in the for if you don't need the correllation of it with it's self(as i guess it should be perfect = 1?)
+            for (int j=0; j<=i;j++){
+
+                correllations[i][j]= correllation(clicks[i].wave, clicks[j].wave);
+            }
+        }
+        return correllations;
     }
 
     double correllation(double[] wave1, double[] wave2){
@@ -32,35 +73,6 @@ public class CorrellationStorage {
             total += wave1[i]*wave2[i];
         }
         return total/wave1.length;
-    }
-
-
-    public class MyKey{
-        private final int x;
-        private final int y;
-
-        public MyKey(int x, int y) {
-            if (x > y) {
-                this.x = y;
-                this.y = x;
-            } else {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof MyKey)) return false;
-            MyKey key = (MyKey) o;
-            return x == key.x && y == key.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return 0;//TODO this will only ever store 1 value?
-        }
     }
 
     class Click{
@@ -73,8 +85,10 @@ public class CorrellationStorage {
 
     Click[] getTestData(int size, int waveLen){
         Click[] clicks = new Click[size];
+        int offset = 100;
         for (int i=0;i<size;i++){
             clicks[i] = new Click(randAry(waveLen));
+            clicks[i].id = i+offset;
         }
         return clicks;
     }
